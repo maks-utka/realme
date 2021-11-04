4b749812fc5c3432e64eff8524d6ac7b9cbf6bd1
mirout <maksimka.prokopets@mail.ru>
2021-11-04 19:02:35 +0700

    home work1

---
+++ js/script.js
@@ -0,0 +1,20 @@
+const number0fFilms = prompt("Сколько Фильмов вы посмотрели?", "");
+
+const obj = {
+    count:(number0fFilms) ,
+    movies: {},
+    actors: {},
+    gengers: arr =[],
+    privat: false
+};
+console.log(obj.count);
+console.log(obj.privat);
+
+
+const a=prompt("какой последний фильм вы посмотрели", ""),
+      b=prompt("Насколько вы его можете оценить?", ""),
+      c=prompt("какой последний фильм вы посмотрели", ""),
+      d=prompt("Насколько вы его можете оценить?", "");
+obj.movies[a]=b;
+obj.movies[c]=d;
+console.log(obj);
